da6ee2371d9bc10b5840bea6525562e592b3de5c
Tanner Gill <tanner.gill@agilebits.com>
2023-05-12 08:13:35 -0700

    http server with a single 'hello there' handler

---
+++ crates/http/src/main.rs
@@ -0,0 +1,25 @@
+use std::{convert::Infallible, net::SocketAddr};
+
+use hyper::server::Server;
+use hyper::{
+    service::{make_service_fn, service_fn},
+    Body, Request, Response,
+};
+
+async fn handle(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new(Body::from("Hello there")))
+}
+
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    let make_service = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle)) });
+
+    let server = Server::bind(&addr).serve(make_service);
+
+    println!("Server listing on {}...", addr);
+    if let Err(e) = server.await {
+        eprintln!("Server failed: {}", e);
+    }
+}
